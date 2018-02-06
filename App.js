b3ae71e7c0265f24ec0d6ad94f8547fc0730ed93
Michael Tran <michaelhuytran@gmail.com>
2018-02-05 20:19:43 -0800

    Navigation and Status Bar

--- App.js
+++ App.js
@@ -1,13 +1,67 @@
 import React from 'react';
 import {
 	View,
-	StyleSheet, SafeAreaView,
+	StyleSheet, SafeAreaView, Platform, StatusBar
 } from 'react-native';
 import AddEntry from "./components/AddEntry";
 import { createStore } from 'redux';
 import { Provider} from 'react-redux';
 import reducer from './reducers'
 import History from "./components/History";
+import { TabNavigator } from 'react-navigation';
+import { purple, white } from "./utils/colors";
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import {Constants} from 'expo';
+
+function UdacisStatusBar({backgroundColor, ...props}){
+	return (
+		<View style={{backgroundColor, height: Constants.statusBarHeight}}>
+			<StatusBar translucent backgroundColor={backgroundColor}
+								 {...props}/>
+		</View>
+	)
+}
+
+const Tabs = TabNavigator({
+	History: {
+		screen: History,
+		navigationOptions : {
+			tabBarLabel: 'History',
+			tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks'
+																						 color={tintColor}
+																						 size={30}/>
+		}
+	},
+	AddEntry: {
+		screen: AddEntry,
+		navigationOptions: {
+			tabBarLabel: 'Add Entry',
+			tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square'
+																						 color={tintColor}
+																						 size={30}/>
+		}
+	}
+}, {
+	navigationOptions: {
+		headers: null
+	}
+	},
+	{
+	tabBarOptions:{
+		activeTintColor: Platform.OS === 'ios' ? purple : white,
+		style: {
+			height: 56,
+			backgroundColor: Platform.OS === 'ios' ? white: purple,
+			shadowColor: 'rgba(0,0,0,0.24)',
+			shadowOffset: {
+				width: 0,
+				height: 3
+			},
+			shadowRadius: 6,
+			shadowOpacity: 1,
+		}
+	}
+});
 
 export default class App extends React.Component {
   state = {
@@ -18,9 +72,8 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
 				<SafeAreaView style={styles.safeArea}>
           <View style={styles.main}>
-						<View style={{height: 20}}/>
-						<History/>
-            {/*<AddEntry/>*/}
+						<UdacisStatusBar backgroundColor={purple} barStyle='light-content'/>
+						<Tabs/>
           </View>
         </SafeAreaView>
       </Provider>

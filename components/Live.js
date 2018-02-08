b8348587f3d5c29d8516fd6df305a985aa72376f
Michael Tran <michaelhuytran@gmail.com>
2018-02-07 21:31:17 -0800

    [feature] - location services

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
-import {View, Text, ActivityIndicator} from 'react-native';
+import {View, Text, ActivityIndicator, StyleSheet, TouchableOpacity} from 'react-native';
+import { Location } from 'expo';
+import { Foundation } from '@expo/vector-icons';
+import {purple, white} from "../utils/colors";
+
+
+
 
 export default class Live extends Component{
 	state = {
@@ -22,8 +28,15 @@ export default class Live extends Component{
 		}
 		if(status === 'undetermined'){
 			return (
-				<View>
-					<Text>Undetermined</Text>
+				<View style={style.center}>
+					<Foundation name='alert' size={50}/>
+					<Text>
+						You need to enable location services for this app.
+					</Text>
+					<TouchableOpacity onPress={this.askPermission}
+														style={styles.button}>
+						<Text style={styles.buttonText}>Enable</Text>
+					</TouchableOpacity>
 				</View>)
 		}
 		return(
@@ -34,3 +47,28 @@ export default class Live extends Component{
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'space-between',
+	},
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: '30',
+		marginRight: '30'
+	},
+	button: {
+		padding: 10,
+		backgroundColor: purple,
+		alignSelf: 'center',
+		borderRadius: 5,
+		margin: 20,
+	},
+	buttonText: {
+		color: white,
+		fontSize: 20,
+	}
+});
